8f3719d3a1ab746096315c7297c94f05db6f283f
andre85silva <andre.silva85@hotmail.com>
2019-07-17 21:48:57 +0100

    Implementei o método main.

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -3,4 +3,8 @@ public class Calculadora {
 	public static int add(int a, int b) {
 		return a + b ;
 	}
+	
+	public static void main (String[] args) {
+		System.out.println("Add 1 + 2 = " + add(1, 2));
+	}
 }
