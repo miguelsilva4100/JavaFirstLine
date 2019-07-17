179a6e2e0d0cdf1a3932ef53fca44b10093aa9ab
Miguel Silva <miguelsilva4100@gmail.com>
2019-07-17 22:07:05 +0100

    Class "Helloooo" aumentada

--- src/Helloooo.java
+++ src/Helloooo.java
@@ -1,9 +1,35 @@
 
+import java.util.Scanner;
+
 public class Helloooo {
 
 	public static void main(String[] args) {
 		System.out.println("Helloooooooo...");
-
+		System.out.println("Qual a idade do Miguel?");
+		
+		String miguel = new String();
+		int idade;
+		
+		do {
+		
+		
+		Scanner s1 = new Scanner(System.in);
+		idade = s1.nextInt();
+		
+		if (idade == 49){
+			System.out.println("Acertaste!!!");
+		}
+		else if (idade < 49 ) {
+			System.out.println("Já não sou tão novo assim...");
+		}
+		
+		else if (idade > 49 ) {
+			System.out.println("Não sou tão velho...");
+		}
+		else 
+			System.out.println("tenta outra vez......");
+		}
+		while (idade < 100 || idade > 0);
 	}
 
 }
